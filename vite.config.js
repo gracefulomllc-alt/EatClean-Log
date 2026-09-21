5289bdd3c81eee38faa64dbbf7c9e2b07868542e
gracefulomllc-alt <gracefulomllc@gmail.com>
2026-09-21 14:35:07 -0600

    Remove Netlify plugin from Vite config
    Removed Netlify plugin from Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import netlify from "@netlify/vite-plugin";
 
-export default defineConfig({ plugins: [react(), netlify()] });
+export default defineConfig({ plugins: [react()] });
